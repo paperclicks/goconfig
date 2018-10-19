834cf6d67b4e759d9ef241754b4974a1bc8527d4
Ervin Hoxha <ervin@paperclicks.net>
2018-10-19 10:53:57 +0200

    added missing New() method

--- goconfig.go
+++ goconfig.go
@@ -1,7 +1,9 @@
-package goconfig
+package config
 
 import (
+	"io"
 	"io/ioutil"
+	"log"
 	"os"
 
 	"gopkg.in/yaml.v2"
@@ -15,7 +17,23 @@ type Value struct {
 
 //Config is a struct that holds all the key:value pairs present in the config file
 type Config struct {
-	Values []Value `yaml:"values"`
+	Values                                              []Value `yaml:"values"`
+	AppLogFile, InfoLogFile, DebugLogFile, ErrorLogFile io.Writer
+}
+
+//New creates a new Config instance and initializes all the necessary configs
+func New(filename string) (*Config, error) {
+
+	//load config.yml to Config data structure
+	config, err := LoadConfig(filename)
+	if err != nil {
+		return &Config{}, err
+	}
+
+	//initialize environment variables from filename if no ENVs were provided on startup
+	InitEnvironment(filename)
+
+	return &config, nil
 }
 
 func saveConfig(c Config, filename string) error {
@@ -27,7 +45,7 @@ func saveConfig(c Config, filename string) error {
 	return ioutil.WriteFile(filename, bytes, 0644)
 }
 
-//LoadConfig loads a .yml config file into the Services data structure
+//LoadConfig loads a .yml config file into the Config data structure
 func LoadConfig(filename string) (Config, error) {
 	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -56,4 +74,27 @@ func SetValuesAsEnv(filename string) error {
 	}
 
 	return nil
+}
+
+//InitEnvironment makes sure to load all ENV variables from config.yml in case not ENV were provided
+func InitEnvironment(filename string) {
+
+	//if ENVIRONMENT env variable is not set or is different from false, load all configs from file
+	//when in production all ENV variables will be set from rancher
+	envValue, isEnvSet := os.LookupEnv("ENVIRONMENT")
+
+	if !isEnvSet {
+		log.Println("ENVIRONMENT is not set! Loading ENV variables from config.yml")
+
+		err := SetValuesAsEnv(filename)
+
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+
+	} else {
+		log.Printf("ENVIRONMENT = %s ! Loading config ENV from system...\n", envValue)
+	}
+
 }
